ab55678cfbd386af73af5389d0ee2814d828fbbe
Vidyadheesh <itcell@git.edu>
2026-07-20 18:11:24 +0530

    sidebar notifications link change

--- src/data/sidebarLinks.js
+++ src/data/sidebarLinks.js
@@ -9,11 +9,11 @@ const sidebarLinks = [
       },
       {
         text: "Convocation List for 2023-24",
-        url: "https://git.edu/wp-content/uploads/2025/03/Convocation_List_202324.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2025/03/Convocation_List_202324.pdf",
       },
       {
         text: "Malpractice List for 2023-24",
-        url: "https://git.edu/wp-content/uploads/2025/03/MPC_Details_202324.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2025/03/MPC_Details_202324.pdf",
       },
       {
         text: "Procedure for Makeup Exam Recommendation",
@@ -61,39 +61,39 @@ const sidebarLinks = [
       },
       {
         text: "CGPA to Percentage Conversion Certificate from 2021 Scheme Onwards",
-        url: "https://git.edu/wp-content/uploads/2024/09/CGPA_To_Percenatge_2021.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2024/09/CGPA_To_Percenatge_2021.pdf",
       },
       {
         text: "Open Day Registration Steps",
-        url: "https://git.edu/wp-content/uploads/2024/11/OpenDay_Registration_HelpFile.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2024/11/OpenDay_Registration_HelpFile.pdf",
       },
       {
         text: "Certificate of Grading System and Medium of Instructions for UG 2021 Batch Onwards",
-        url: "https://git.edu/wp-content/uploads/2025/01/CRGP_Table_UG_2021.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2025/01/CRGP_Table_UG_2021.pdf",
       },
       {
         text: "Certificate of Grading System and Medium of Instructions for PG 2022 Scheme Onwards",
-        url: "https://git.edu/wp-content/uploads/2025/01/CRGP_Table_PG_2022.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2025/01/CRGP_Table_PG_2022.pdf",
       },
       {
         text: "Autonomous Rules and Regulations for 2021-22 Batch (UG)",
-        url: "https://git.edu/wp-content/uploads/2025/03/Rules_Regulations_2021_UG.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2025/03/Rules_Regulations_2021_UG.pdf",
       },
       {
         text: "Autonomous Rules and Regulations for 2020-21 and Onward Batches (PG)",
-        url: "https://git.edu/wp-content/uploads/2025/03/Rules_Regulations_2020_PG.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2025/03/Rules_Regulations_2020_PG.pdf",
       },
       {
         text: "Autonomous Rules and Regulations for 2022-23 and Onward Batches (UG)",
-        url: "https://git.edu/wp-content/uploads/2025/03/Rules_Regulations_2022_UG.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2025/03/Rules_Regulations_2022_UG.pdf",
       },
       {
         text: "Autonomous Rules and Regulations for 2022-23 and Onward Batches (PG)",
-        url: "https://git.edu/wp-content/uploads/2025/03/Rules_Regulations_2022_PG.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2025/03/Rules_Regulations_2022_PG.pdf",
       },
       {
         text: "Revised Rules and Regulations for 2024-25 Admitted Batch",
-        url: "https://git.edu/wp-content/uploads/2026/02/RevisedRegulations_2024Batch.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2026/02/RevisedRegulations_2024Batch.pdf",
       },
     ],
   },
@@ -102,11 +102,11 @@ const sidebarLinks = [
     items: [
       {
         text: "Challenge Revaluation Applying Procedure",
-        url: "https://git.edu/wp-content/uploads/2024/11/CRV_Registration_HelpFile.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2024/11/CRV_Registration_HelpFile.pdf",
       },
       {
         text: "Makeup Exam Registration Procedure",
-        url: "https://git.edu/wp-content/uploads/2025/02/MK_Registration_HelpFile.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2025/02/MK_Registration_HelpFile.pdf",
       },
       {
         text: "Re-Registration Procedure",
@@ -114,27 +114,27 @@ const sidebarLinks = [
       },
       {
         text: "Open Day Registration Procedure",
-        url: "https://git.edu/wp-content/uploads/2024/11/OpenDay_Registration_HelpFile.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2024/11/OpenDay_Registration_HelpFile.pdf",
       },
       {
         text: "Backlog Courses Registration Procedure",
-        url: "https://git.edu/wp-content/uploads/2025/01/Backlog_Registration_HelpFile.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2025/01/Backlog_Registration_HelpFile.pdf",
       },
       {
         text: "Revaluation Registration Procedure",
-        url: "https://git.edu/wp-content/uploads/2025/02/Revaluation_Registration_HelpFile.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2025/02/Revaluation_Registration_HelpFile.pdf",
       },
       {
         text: "Faculty Feedback Procedure",
-        url: "https://git.edu/wp-content/uploads/2025/05/FeedbackSteps.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2025/05/FeedbackSteps.pdf",
       },
       {
         text: "FastTrack Re-Registration Procedure",
-        url: "https://git.edu/wp-content/uploads/2025/08/FastTack_Registration_HelpFile.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2025/08/FastTack_Registration_HelpFile.pdf",
       },
       {
         text: "Regular Course Registration Procedure",
-        url: "https://git.edu/wp-content/uploads/2024/08/Course-Registration_Steps.pdf",
+        url: "https://2025.git.edu/wp-content/uploads/2024/08/Course-Registration_Steps.pdf",
       },
     ],
   },
